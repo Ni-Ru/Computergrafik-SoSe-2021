f23f319d24484b938324066c8094e78aa111acb9
Nic <nic.rubner@hs-furtwangen.de>
2021-06-26 01:16:17 +0200

    11. Picking/Assets

--- Tut11_AssetsPicking/Tut11_AssetsPicking.cs
+++ Tut11_AssetsPicking/Tut11_AssetsPicking.cs
@@ -21,7 +21,22 @@ namespace FuseeApp
     {
         private SceneContainer _scene;
         private SceneRendererForward _sceneRenderer;
+        private float _camAngle = 0;
         private Transform _baseTransform;
+        private SurfaceEffect _rightRearEffect;
+        private Transform _canonrotate;
+        private Transform _front;
+        private Transform _rear;
+        private SceneNode body;
+        private SceneNode rotatecanons;
+        private SceneNode rotate;
+        private ScenePicker _scenePicker;
+        private float clickCheck = 0;
+        private PickResult _currentPick;
+        private float4 _oldColor;
+        private SceneNode canon;
+        private SceneNode steering;
+
 
 
         SceneContainer CreateScene()
@@ -64,25 +79,101 @@ namespace FuseeApp
         {
             RC.ClearColor = new float4(0.8f, 0.9f, 0.7f, 1);
 
-            _scene = CreateScene();
+            _scene = AssetStorage.Get<SceneContainer>("vehicle.fus");
+
+            _baseTransform = _scene.Children.FindNodes((node) => node.Name == "Body")?.FirstOrDefault()?.GetTransform();
+
+            _rightRearEffect = _scene.Children.FindNodes(node => node.Name == "Body")?.FirstOrDefault()?.GetComponent<SurfaceEffect>();
+
+            _canonrotate = _scene.Children.FindNodes(node => node.Name == "rotatecanons")?.FirstOrDefault()?.GetTransform();
+            rotatecanons = _scene.Children.FindNodes(node => node.Name == "rotatecanons")?.FirstOrDefault();
+            rotate = _scene.Children.FindNodes(node => node.Name == "Window.001")?.FirstOrDefault();
+            canon = _scene.Children.FindNodes(node => node.Name == "canonhull")?.FirstOrDefault();
+
+            _front = _scene.Children.FindNodes(node => node.Name == "front-turn")?.FirstOrDefault()?.GetTransform();
+
+            _rear = _scene.Children.FindNodes(node => node.Name == "back-turn")?.FirstOrDefault()?.GetTransform();
+
+            body = _scene.Children.FindNodes(node => node.Name == "Body")?.FirstOrDefault();
+            steering = _scene.Children.FindNodes(node => node.Name == "steering")?.FirstOrDefault();
+
+
+
+
+            //_baseTransform.Rotation.y=M.Pi/2;
+
 
             // Create a scene renderer holding the scene above
             _sceneRenderer = new SceneRendererForward(_scene);
-        }
 
-        // RenderAFrame is called once a frame
+            _scenePicker = new ScenePicker(_scene);
+        }
         // RenderAFrame is called once a frame
         public override void RenderAFrame()
         {
             SetProjectionAndViewport();
 
-            _baseTransform.Rotation = new float3(0, M.MinAngle(TimeSinceStart), 0);
-
             // Clear the backbuffer
             RC.Clear(ClearFlags.Color | ClearFlags.Depth);
 
             // Setup the camera 
-            RC.View = float4x4.CreateTranslation(0, 0, 40) * float4x4.CreateRotationX(-(float) Math.Atan(15.0 / 40.0));
+            RC.View = float4x4.CreateTranslation(0, 0, 40) * float4x4.CreateRotationX(-(float)Math.Atan(15.0 / 40.0));
+
+            if (Mouse.LeftButton)
+            {
+                float2 pickPosClip = Mouse.Position * new float2(2.0f / Width, -2.0f / Height) + new float2(-1, 1);
+
+                PickResult newPick = _scenePicker.Pick(RC, pickPosClip).OrderBy(pr => pr.ClipPos.z).FirstOrDefault();
+
+                if (newPick?.Node != _currentPick?.Node)
+                {
+                     if (_currentPick != null)
+                    {
+                        var ef = _currentPick.Node.GetComponent<DefaultSurfaceEffect>();
+                        ef.SurfaceInput.Albedo = _oldColor;
+                    }
+                    if (newPick != null)
+                    {
+                        if (newPick.Node == rotatecanons || newPick.Node == rotate || newPick.Node ==canon)
+                        {
+                            clickCheck = 1;
+                        }
+                        if (newPick.Node == body || newPick.Node == steering)
+                        {
+                            clickCheck = 2;
+                        }
+                         var ef = newPick.Node.GetComponent<SurfaceEffect>();
+                        _oldColor = ef.SurfaceInput.Albedo;
+                        ef.SurfaceInput.Albedo = (float4) ColorUint.OrangeRed;
+                    }
+                    _currentPick = newPick;
+                }
+
+            }
+            if (clickCheck == 1)
+            {
+                _canonrotate.Rotation.y += Keyboard.ADAxis * 0.02f;
+                _canonrotate.Rotation.z = Keyboard.WSAxis * 0.1f;
+
+
+            }
+            if (clickCheck == 2)
+            {
+                //wheels forward / backwards
+                float drive = _rear.Rotation.z;
+                drive += (-Keyboard.WSAxis) * DeltaTime;
+                _rear.Rotation.z = drive;
+                _front.Rotation.z = drive;
+
+                //wheels turning
+                _rear.Rotation.y = Keyboard.ADAxis * (-0.2f);
+                _front.Rotation.y = Keyboard.ADAxis * 0.2f;
+
+                //move car
+                float move = _baseTransform.Translation.x;
+                move += Keyboard.WSAxis * DeltaTime;
+                _baseTransform.Translation.x = move;
+            }
 
             // Render the scene on the current render context
             _sceneRenderer.Render(RC);
@@ -104,6 +195,6 @@ namespace FuseeApp
             // Back clipping happens at 2000 (Anything further away from the camera than 2000 world units gets clipped, polygons will be cut)
             var projection = float4x4.CreatePerspectiveFieldOfView(M.PiOver4, aspectRatio, 1, 20000);
             RC.Projection = projection;
-        }                
+        }
     }
 }
